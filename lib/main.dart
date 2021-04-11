6e7baf27c44f337de80cc40c0b31e261759f3423
Silas Santana <silas.santos@lojainterativa.com>
2021-04-11 10:49:23 -0300

    Adding images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,7 +23,9 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'RobotoCondensed',
                   fontWeight: FontWeight.bold))),
       home: CategoriesScreen(),
-      routes: {'/category-meals': (context) => CategoryMealsScreen()},
+      routes: {
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen()
+      },
     );
   }
 }
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals.elementAt(index).title,
+              imageUrl: categoryMeals.elementAt(index).imageUrl,
+              affordability: categoryMeals.elementAt(index).affordability,
+              complexity: categoryMeals.elementAt(index).complexity,
+              duration: categoryMeals.elementAt(index).duration);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
